15ee8ee35befd74275630ad9c14f58f7eed1a4b4
RamakrushnaBiswal <125277258+RamakrushnaBiswal@users.noreply.github.com>
2024-12-14 13:46:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,2 @@
+# CartoonNetwork-90s
+ A Codedex Holiday Hackathon Project
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+              p1: "#293774",
+              p2: "#9051A2",
+              p3: "#FA5D66",
+              p4: "#FFA1C5",
+              p5: "#FDFEEC",  
+            },
+            fontFamily: {
+                ashington: ["ashington", "sans"],
+                crang: ["crang", "sans"],
+            }
+        }
+    }
+}
